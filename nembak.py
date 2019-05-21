b24223a46c7e1626e4a051b565fab42036e921e1
WahyuSibran <50877597+WahyuSibran@users.noreply.github.com>
2019-05-21 22:11:19 +0700

    Add files via upload

---
+++ nembak.py
@@ -0,0 +1,14 @@
+Nembak Tool
+1 _ apt update
+
+2 _ apt upgrade
+
+3 _ pkg install python2
+
+4 _ pkg install git
+
+5 _ git clone https://github.com/Xeit666h05t/Nembak-Tools.git
+
+6 _ cd Nembak-Tools
+
+7 _ python2 nembak.py
